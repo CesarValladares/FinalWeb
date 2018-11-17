d7c1a02c6e242ada62ed22953fcfdf9ea8a0fa4e
Eric Parton <partonemp@gmail.com>
2018-11-17 17:37:35 -0600

    book.html ahora responsivo

---
+++ FrontOfficeFront/js/bookScript.js
@@ -0,0 +1,16 @@
+document.getElementById("userNavigationMobileDropDownMenu").style.display = "none";
+function toggleDropdown() {
+	var x = document.getElementById("userNavigationMobileDropDownMenu");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn') && !e.target.matches('.dropglyph')) {
+    document.getElementById("userNavigationMobileDropDownMenu").style.display = "none";
+  }
+}
