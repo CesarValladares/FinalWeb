0a9f543022a02227a6fac5c1ec433ecb68843b5d
LuisAriasC <luis.ac945@gmail.com>
2018-11-27 20:59:54 -0600

    clients backoffice

--- api/models/rent.js
+++ api/models/rent.js
@@ -12,29 +12,11 @@ var RentSchema = Schema({
     type: Schema.ObjectId,
     ref: 'Client'
   },
-  total_books: Number,
-  books: {
-      book1: {
-        type: Schema.ObjectId,
-        ref: 'Book'
-      },
-      book2: {
-        type: Schema.ObjectId,
-        ref: 'Book'
-      },
-      book3: {
-        type: Schema.ObjectId,
-        ref: 'Book'
-      },
-      book4: {
-        type: Schema.ObjectId,
-        ref: 'Book'
-      },
-      book5: {
-        type: Schema.ObjectId,
-        ref: 'Book'
-      }
+  book: {
+    type: Schema.ObjectId,
+    ref: 'Book'
   }
+  
 });
 
 module.exports = mongoose.model('Rent', RentSchema);
