e2254ca8da26e38300241553e5ff763777adc5d0
LuisAriasC <luis.ac945@gmail.com>
2018-11-27 18:06:22 -0600

    books

--- frontend/src/app/components/books/books.component.ts
+++ frontend/src/app/components/books/books.component.ts
@@ -16,19 +16,61 @@ export class BooksComponent implements OnInit {
   constructor(private bookService: BookService) { }
 
   ngOnInit() {
-    console.log('INICIANDO LIBROS');
     this.getBooks();
+  }
+
+  addBook(form: NgForm) {
+    if(form.value._id){
+      this.bookService.putBook(form.value)
+        .subscribe(res => {
+          this.resetForm(form);
+          M.toast({html: 'Updated Successfully'});
+          this.getBooks();
+        });
+    }else{
+      console.log('CREATE');
+      this.bookService.postBook(form.value)
+      .subscribe(res => {
+        this.resetForm(form);
+        M.toast({html: 'Save Successfully'});
+        this.getBooks();
+      }, err => {
+        alert("El libro no se creo correctamente");
+      });
+    }
   }
 
   getBooks(){
     this.bookService.getBooks()
       .subscribe(
         response => {
-        this.bookService.books = response as Books[];
+        this.bookService.books = response as Book[];
         console.log(response);
       } , error => {
         console.log(error);
       });
+  }
+
+  editBook(book: Book){
+    this.bookService.selectedBook = book;
+  }
+
+  deleteBook(_id: string){
+    if(confirm('Are you sure you want to delete it?')){
+      console.log(_id);
+      this.bookService.deleteBook(_id)
+        .subscribe(res => {
+          M.toast({html: 'Book Deleted'});
+          this.getBooks();
+        });
+    }
+  }
+
+  resetForm(form?: NgForm) {
+    if(form){
+      form.reset();
+      this.bookService.selectedBook = new Book;
+    }
   }
 
 }
